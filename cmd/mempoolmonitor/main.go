98b93727420c25d3ddbf717db69e5a5710a0829d
Mary <135226561+mpermar082@users.noreply.github.com>
2025-10-02 18:49:18 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/mempoolmonitor/main.go
@@ -0,0 +1,23 @@
+// cmd/mempoolmonitor/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "mempoolmonitor/internal/mempoolmonitor"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := mempoolmonitor.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
